ee2589a82b7f7f755594f1c7003fd3ba1afc000c
Fabiana <chumbafabina@gmail.com>
2024-12-15 16:01:49 -0300

    probando commits

--- clase12/ejercicio2.js
+++ clase12/ejercicio2.js
@@ -2,3 +2,4 @@
 //Escribe un programa que solicite al usuario ingresar una contraseña. Si la
 //contraseña es incorrecta, debe volver a pedirla hasta que el usuario
 //ingrese la correcta. La contraseña correcta es "1234".
+console.log("Hola");
